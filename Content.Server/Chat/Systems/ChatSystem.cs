dfae97adb7854309b2a4edd6d8229597d50967ad
JamboreeBot <JamboreeBot@proton.me>
2025-09-06 07:24:14 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -92,6 +92,7 @@
 // SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
 // SPDX-FileCopyrightText: 2025 Ilya246 <57039557+Ilya246@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 IrisTheAmped <iristheamped@gmail.com>
+// SPDX-FileCopyrightText: 2025 JamboreeBot <JamboreeBot@proton.me>
 // SPDX-FileCopyrightText: 2025 John Willis <143434770+CerberusWolfie@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Mnemotechnican <69920617+Mnemotechnician@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Piras314 <p1r4s@proton.me>
